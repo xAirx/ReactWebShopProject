90f1b22343db5ec41c58c5013a2acc79e4c6f4b8
xAirx <marcowurtz@hotmail.com>
2020-05-13 22:55:33 +0200

    up

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './homepage.styles.scss';
 function App() {
   return (
     <div>
-      <h1>Webshop - Project - Test-testx</h1>
+      <h1>Webshop - Project - Test-testxx</h1>
       <Homepage />
     </div>
   );
