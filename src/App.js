ffc9282784afe6e6a9f2793f0ccd1a7bb093ebae
xAirx <marcowurtz@hotmail.com>
2020-05-14 14:04:10 +0200

    trying out sentry

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React from 'react';
 import './App.css';
 import Homepage from './HomepageComponent';
 import './homepage.styles.scss';
+import * as Sentry from '@sentry/node';
+// or using CommonJS
+// const Sentry = require('@sentry/node');
+
+Sentry.init({ dsn: 'https://9f680323fb824097b101a1ec5c11c83d@o392677.ingest.sentry.io/5240593' });
 
 function App() {
+  myUndefinedFunction();
   return (
     <div>
       <h1>Webshop - Project - Test-testxx</h1>
