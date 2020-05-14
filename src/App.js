493dfd4966831420d324a5e6a9e96d71ce9c629d
xAirx <marcowurtz@hotmail.com>
2020-05-14 17:32:58 +0200

    added sentry to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './App.css';
+import * as Sentry from '@sentry/node';
+
 import Homepage from './HomepageComponent';
 import './homepage.styles.scss';
+// or using CommonJS
+// const Sentry = require('@sentry/node');
+
+Sentry.init({ dsn: 'https://0279e0a3aae840339f4a711848494919@o392672.ingest.sentry.io/5240589' });
 /* import TriggerSentry from './TriggerSentry'; */
 
 function App() {
