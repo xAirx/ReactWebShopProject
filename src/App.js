b9cfc3aa0cd3237673689c628a12a45424f4402c
xAirx <marcowurtz@hotmail.com>
2020-05-14 14:09:49 +0200

    trying to trigger sentry

--- src/App.js
+++ src/App.js
@@ -9,11 +9,10 @@ import * as Sentry from '@sentry/node';
 Sentry.init({ dsn: 'https://9f680323fb824097b101a1ec5c11c83d@o392677.ingest.sentry.io/5240593' });
 
 function App() {
-  myUndefinedFunction();
   return (
     <div>
       <h1>Webshop - Project - Test-testxx</h1>
-      <Homepage />
+      <Homepage /////>
     </div>
   );
 }
