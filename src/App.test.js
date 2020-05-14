97328b86910413580926e8878044e14e07dea6e8
xAirx <marcowurtz@hotmail.com>
2020-05-14 12:12:27 +0200

    GithubActionsTestPush

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, getAllByText, getAllByDisplayValue } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders h1', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Webshop - Project - Test-testxx/i);
   expect(linkElement).toBeInTheDocument();
 });
