a9a5dff7e64e89143e7f7f9dc4043280dfe83761
xAirx <marcowurtz@hotmail.com>
2020-05-15 21:11:20 +0200

    Sentry updated  Logrocket monitoring added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+/* eslint-disable react/button-has-type */
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import './App.css';
-import * as Sentry from '@sentry/node';
-
-import Homepage from './HomepageComponent';
+/* import { applyMiddleware, createStore } from 'redux'; */
 import './homepage.styles.scss';
-// or using CommonJS
-// const Sentry = require('@sentry/node');
 
-Sentry.init({ dsn: 'https://0279e0a3aae840339f4a711848494919@o392672.ingest.sentry.io/5240589' });
+import * as Sentry from '@sentry/browser';
+// or using CommonJS
+import LogRocket from 'logrocket';
+import setupLogRocketReact from 'logrocket-react';
+import Homepage from './HomepageComponent';
+
+LogRocket.init('8ckc5m/my-master-project-dev');
+// after calling LogRocket.init()
+setupLogRocketReact(LogRocket);
+
 /* import TriggerSentry from './TriggerSentry'; */
+Sentry.configureScope(scope => {
+  scope.setExtra('sessionURL', LogRocket.sessionURL);
+});
+
+LogRocket.getSessionURL(sessionURL => {
+  Sentry.configureScope(scope => {
+    scope.setExtra('sessionURL', sessionURL);
+  });
+});
+
+// after calling LogRocket.init()
+/* setupLogRocketReact(LogRocket);
+ */
+Sentry.init({ dsn: 'https://0279e0a3aae840339f4a711848494919@o392672.ingest.sentry.io/5240589' });
 
 function App() {
+  /*   const methodDoesNotExist = () => {
+    console.log('wtf');
+    methodExistsNot();
+  }; */
+
   return (
     <div>
-      <h1>Webshop - Projectx</h1>
+      <h1>Webshop - DevBranch</h1>
       <Homepage />
+      {/* <button onClick={methodDoesNotExist}>Test Sentry</button>; */}
     </div>
   );
 }
--- src/log.js
+++
@@ -1,7 +0,0 @@
-import * as Sentry from '@sentry/node';
-// or using CommonJS
-// const Sentry = require('@sentry/node');
-
-Sentry.init({ dsn: 'https://9f680323fb824097b101a1ec5c11c83d@o392677.ingest.sentry.io/5240593' });
-
-module.exports = Sentry;
