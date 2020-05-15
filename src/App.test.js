f68b77df9f601fe080ee099c2d32796c4b1505fc
xAirx <marcowurtz@hotmail.com>
2020-05-15 21:40:03 +0200

    Push New Dev and Prod yml to master

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders h1', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Webshop - Project/);
+  const linkElement = getByText(/Webshop - MasterBranch/);
   expect(linkElement).toBeInTheDocument();
 });
