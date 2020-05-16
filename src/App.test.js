66dd781bd901f96250f596c678c5a5c2dba942f5
xAirx <marcowurtz@hotmail.com>
2020-05-16 03:53:41 +0200

    yml test heroku deploy problem....

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders h1', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Webshop - MasterBranch/);
+  const linkElement = getByText(/Webshop - Development/);
   expect(linkElement).toBeInTheDocument();
 });
