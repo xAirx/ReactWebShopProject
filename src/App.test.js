08b7d43151fec3ab106b4c5986ff0b164855662d
xAirx <marcowurtz@hotmail.com>
2020-05-16 15:21:14 +0200

    test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders h1', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Webshop - Developmentx/);
+  const linkElement = getByText(/Webshop - DevelopmentServerx/);
   expect(linkElement).toBeInTheDocument();
 });
