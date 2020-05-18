b31c427ca0aa273f372d7b45e335c1706bcc4a6a
xAirx <marcowurtz@hotmail.com>
2020-05-18 18:54:34 +0200

    added old config for terraform basic auth protection with kong

--- routes.tf
+++ routes.tf
@@ -1,41 +1,46 @@
 resource "kong_service" "react" {
-  name     = "create-react-app"
-  protocol = "http"
-  host     = "127.0.0.1"
-  port     = 3000
+  name     = "create-react-app"
+  protocol = "http"
+  host     = "127.0.0.1"
+  port     = 3000
 }
-
-
-variable "basic_password" {
-  type = "string"
-}
-
 
 resource "kong_route" "web_root" {
-  protocols  = ["https", "http"]
-  paths      = ["/"]
-  service_id = "${kong_service.react.id}"
+  protocols  = ["https", "http"]
+  paths      = ["/"]
+  service_id = "${kong_service.react.id}"
+}
+provider "random" {
+  version = "~> 2.0"
+}
+
+resource "random_id" "private_access_password" {
+  byte_length = 32
+}
+
+output "private_access_password" {
+  value = "${random_id.private_access_password.b64_url}"
 }
 
 resource "kong_plugin" "react_basic_auth" {
-  name        = "basic-auth"
-  service_id  = "${kong_service.react.id}"
+  name        = "basic-auth"
+  service_id  = "${kong_service.react.id}"
 
-  config = {
-    hide_credentials = "true"
-  }
+  config = {
+    hide_credentials = "true"
+  }
 }
 
 resource "kong_consumer" "private_access" {
-  username = "private"
+  username = "private"
 }
 
 resource "kong_consumer_plugin_config" "private_access_credentials" {
-  consumer_id = "${kong_consumer.private_access.username}"
-  plugin_name = "basic-auth"
+  consumer_id = "${kong_consumer.private_access.username}"
+  plugin_name = "basic-auth"
 
-  config = {
-    username = "private"
-    password = "${var.basic_password}"
-  }
+  config = {
+    username = "private"
+    password = "${random_id.private_access_password.b64_url}"
+  }
 }
